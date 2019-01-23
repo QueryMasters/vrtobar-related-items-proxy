812a5b54d153409f2c7dcd46e14b82c36c1357df
Victor Raul Tobar <victor@victortobar.com>
2019-01-23 11:04:19 -0800

    Updated proxy server for SDC

--- proxy/server.js
+++ proxy/server.js
@@ -1,52 +1,28 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const morgan = require('morgan');
 const port = 3000;
 
 const app = express();
-app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/api/products', (req, res) => {
-  axios.get('http://54.83.144.162:3002/api/products', {
-    params: req.query,
-  })
-  .then(product => res.status(200).send(product.data))
-  .catch(() => console.log('ERROR IN PROXY SERVER /API/PRODUCTS'));
-});
-
-app.get('/api/items/:itemId', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/items/${ req.params.itemId }`)
+app.get('/products/:id', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/products/${ req.params.id }`)
   .then(items => res.status(200).send(items.data))
-  .catch(() => console.log('ERROR IN PROXY SERVER /API/ITEMS/:ITEMID'));
+  .catch(() => console.log('ERROR IN PROXY SERVER /API/ITEMS/:id'));
 });
 
-app.get('/api/related/:itemId', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/related/${ req.params.itemId }`)
+app.get('/related/:id', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/related/${ req.params.id }`)
   .then(relatedItems => res.status(200).send(relatedItems.data))
-  .catch(() => console.log('ERROR IN PROXY SERVER /API/RELATED/:ITEMID'));
+  .catch(() => console.log('ERROR IN PROXY SERVER /API/RELATED/:id'));
 });
 
-app.get('/api/frequent/:itemId', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/frequent/${ req.params.itemId }`)
+app.get('/frequent/:id', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/frequent/${ req.params.id }`)
   .then(frequentlyTogether => res.status(200).send(frequentlyTogether.data))
-  .catch(() => console.log('ERROR IN PROXY SERVER /API/FREQUENT/:ITEMID'));
-});
-
-app.post('/api/messages', (req, res) => {
-  // axios.post('http://localhost:8888/api/messages', {
-    
-  // })
-});
-
-app.get('/reviews/:productId', (req, res) => {
-  axios.get(`http://ec2-54-153-13-213.us-west-1.compute.amazonaws.com:3003/reviews/${ req.params.productId }`, {
-    params: req.query,
-  })
-  .then(reviewsData => res.status(200).send(reviewsData.data))
-  .catch(() => console.log('ERROR IN PROXY SERVER /REVIEWS/:PRODUCTID'))
+  .catch(() => console.log('ERROR IN PROXY SERVER /API/FREQUENT/:id'));
 });
 
 app.listen(port, (err) => {
