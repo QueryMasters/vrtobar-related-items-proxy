d1055bf179bd1e8d18fd30f4856540d934e652e7
Victor Raul Tobar <victor@victortobar.com>
2019-01-23 11:55:21 -0800

    Update IP addresses

--- proxy/server.js
+++ proxy/server.js
@@ -8,19 +8,19 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/products/:id', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/products/${ req.params.id }`)
+  axios.get(`http:13.58.45.58:1337/products/${ req.params.id }`)
   .then(items => res.status(200).send(items.data))
   .catch(() => console.log('ERROR IN PROXY SERVER /API/ITEMS/:id'));
 });
 
 app.get('/related/:id', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/related/${ req.params.id }`)
+  axios.get(`http://13.58.45.58:1337/api/related/${ req.params.id }`)
   .then(relatedItems => res.status(200).send(relatedItems.data))
   .catch(() => console.log('ERROR IN PROXY SERVER /API/RELATED/:id'));
 });
 
 app.get('/frequent/:id', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/frequent/${ req.params.id }`)
+  axios.get(`http://13.58.45.58:1337/api/frequent/${ req.params.id }`)
   .then(frequentlyTogether => res.status(200).send(frequentlyTogether.data))
   .catch(() => console.log('ERROR IN PROXY SERVER /API/FREQUENT/:id'));
 });
